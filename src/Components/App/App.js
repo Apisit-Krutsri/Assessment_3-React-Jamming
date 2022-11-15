5cb2d2cd9ad37159f8d66cccba8e83d70c77c8cf
Apisit Krutsri <k.apisit@outlook.com>
2022-11-15 11:58:41 +0700

    Finished task 28 but task 27 is not complete

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,23 @@
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-      <div className="App">
-        {/* <SearchBar /> */}
-        <div className="App-playlist">
-          {/* <SearchResults /> */}
-          {/* <Playlist /> */}
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          {/* <SearchBar component /> */}
+          <div className="App-playlist">
+            {/* <Add a SearchResults component /> */}
+            {/* <Add a Playlist component /> */}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
